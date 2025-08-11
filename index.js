bc5e1a84ce2d41b4de5ef97132cc3bf23547c962
ZotNoah <unverzagt.noah@gmail.com>
2025-08-10 22:23:37 -0500

    see if this works
    rootMargin updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const faders = document.querySelectorAll(".fade-in");
 
 const appearOptions = {
     threshold: 0.3,
-    rootMargin: "0px 0px -50px 0px"
+    rootMargin: "0px 0px -200px 0px"
  };
 
 const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
